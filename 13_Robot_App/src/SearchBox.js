baffae38cbbd488bb203a9f3ead25b0a707b034c
Joao Santos <ctw01895@criticaltechworks.com>
2022-06-15 17:56:28 +0200

    Doing the RobotFriends project.

---
+++ 13_Robot_App/src/SearchBox.js
@@ -0,0 +1,8 @@
+const SearchBox = () => {
+    return (
+        <div>
+            <input type='search' placeholder='a' className='pa3 ba b--green bg-lightest-blue'/>
+        </div>
+    );
+}
+export default SearchBox;
