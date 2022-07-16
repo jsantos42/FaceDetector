8117c2faf6faf983bac8bb24dfb94e22528a2a80
jsantos <jsantos@student.42lisboa.com>
2022-07-16 17:13:22 +0100

    Now using Particles background.

---
+++ 17_Face_Recognition/src/components/rank/Rank.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+const Rank = () => {
+    return (
+        <div className={'rank center'}>
+            <p>{'User, your current rank is ...'}</p>
+            <p>{'#5'}</p>
+        </div>
+    )
+
+}
+
+export default Rank;
--- 17_Face_Recognition/src/containers/App.js
+++ 17_Face_Recognition/src/containers/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Nav from "../components/nav/Nav";
 import Logo from "../components/logo/Logo";
 import ImageLinkForm from "../components/imageLinkForm/ImageLinkForm";
+import Rank from "../components/rank/Rank";
+import ParticlesBg from "../components/ParticlesBg";
 
 function App() {
     return (
         <div className="app">
+            <ParticlesBg />
             <Nav />
             <Logo />
+            <Rank />
             <ImageLinkForm />
             {/*<FaceRecognition />*/}
         </div>
