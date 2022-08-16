f423dc9e93fce516fb6e6b191d02ff4260620180
jsantos <jsantos@student.42lisboa.com>
2022-08-16 20:17:17 +0100

    Refactored register and entries endpoints.

--- Face Detector (fullstack)/backend/server.js
+++ Face Detector (fullstack)/backend/server.js
@@ -88,30 +88,45 @@ app.get('/profile/:id', (req, res) => {
  *	@return the newly registered user
  */
 /*
-(1) respond with the registered user, thanks to the 'returning' method above
-(2) catches the error when you try to register the same user twice. However,
-you do NOT want to return the 'err' object, because you'd be revealing that
-that user is already registered on your db! So, just return a vague string.
+(1) Transactions make sure that, when doing multiple operations on a database,
+if one fails they all fail, avoiding inconsistencies in a database (for example,
+between tables). It should be used if you need to do operations that are related
+(in this example, when adding a user to both the 'users' and 'login' table)
+(2) the into(<table>) is the same as trx(<table>)
+(3) makes sure it returns the whole row of the inserted user
+(4) makes sure the transaction is executed if every operation went well.
+Otherwise, rollback() undoes any changes to the database.
+(5) catches the error when you try to register the same user twice. However, you
+do NOT want to return the 'err' object, because you'd be revealing that that
+user is already registered on your db! So, just return a vague string.
  */
 app.post('/register', (req, res) => {
 	const {email, name, password} = req.body;
-	
-	// I'm having trouble to retrieve the hash from this promise
-	// bcrypt.hash(password, saltRounds, (err, hash) => {
-	// 	console.log(hash)
-	// })
-	
-	db('users')
-		.returning(['*'])									// (1)
-		.insert({
-			name: name,
-			email: email,
-			joined: new Date(),
-		}).then(user => res.json(user[0]))               	// (1)
+	bcrypt.hash(password, saltRounds, (err, hash) => {
+		db.transaction(trx => trx									// (1)
+			.insert({
+				email: email,
+				hash: hash,
+			})
+			.into('login')											// (2)
+			.returning('email')
+			.then(emailObjArray => trx
+				.insert({
+					name: name,
+					email: emailObjArray[0].email,
+					joined: new Date(),
+				})
+				.into('users')										// (2)
+				.returning(['*'])									// (3)
+				.then(user => res.json(user[0]))
+			)
+			.then(trx.commit)										// (4)
+			.catch(trx.rollback)
+		)
 		.catch(err => res
 			.status(400)
-			.json('unable to register'));				// (2)
-	
+			.json('unable to register'));						// (5)
+		})
 })
 
 /**
@@ -137,20 +152,22 @@ app.post('/signIn', (req, res) => {
 })
 
 
-
-app.put('/image', (req, res) => {
-	const {id} = req.body;
-	
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === Number(id)) {
-			found = true
-			user.entries++;
-			res.json(user.entries);
-		}
-	})
-	if (!found)
-		res.status(404).json('no such user')
+/**
+ *	INCREMENT THE ENTRIES VALUE OF THE CURRENT USER
+ *	Endpoint: '/entries'
+ *	Method: PUT
+ *	Body: json object that has the id of the current user
+ *	@return the updated value of entries
+ */
+app.put('/entries', (req, res) => {
+	db('users')
+		.returning('entries')
+		.where('id', req.body.id)
+		.increment('entries', 1)
+		.then(userEntries => {
+			if (userEntries.length)
+				res.json(userEntries[0].entries);
+			else
+				res.status(400).json('unable to get entries');
+		});
 })
-
-
