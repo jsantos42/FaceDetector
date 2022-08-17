46328cc158babb8b5e1cbc23dfc04cf99e0b12f5
jsantos <jsantos@student.42lisboa.com>
2022-08-17 18:17:03 +0100

    Protected backend against empty input.

--- Face Detector (fullstack)/backend/controllers/register.js
+++ Face Detector (fullstack)/backend/controllers/register.js
@@ -13,32 +13,35 @@ user is already registered on your db! So, just return a vague string.
  */
 const register = (req, res, db, bcrypt, saltRounds) => {
 	const {email, name, password} = req.body;
-	bcrypt.hash(password, saltRounds, (err, hash) => {
-		db.transaction(trx => trx									// (1)
-			.insert({
-				email: email,
-				hash: hash,
-			})
-			.into('login')											// (2)
-			.returning('email')
-			.then(emailObjArray => trx
-				.insert({
-					name: name,
-					email: emailObjArray[0].email,
-					joined: new Date(),
-				})
-				.into('users')										// (2)
-				.returning(['*'])									// (3)
-				.then(user => res.json(user[0]))
+	if (!email || !name || !password)
+		res.status(400).json('Every field must be filled in.');
+	else
+		bcrypt.hash(password, saltRounds, (err, hash) => {
+			db.transaction(trx => trx									// (1)
+				 .insert({
+					 email: email,
+					 hash: hash,
+				 })
+				 .into('login')											// (2)
+				 .returning('email')
+				 .then(emailObjArray => trx
+					 .insert({
+						 name: name,
+						 email: emailObjArray[0].email,
+						 joined: new Date(),
+					 })
+					 .into('users')										// (2)
+					 .returning(['*'])									// (3)
+					 .then(user => res.json(user[0]))
+				 )
+				 .then(trx.commit)										// (4)
+				 .catch(trx.rollback)
 			)
-			.then(trx.commit)										// (4)
-			.catch(trx.rollback)
-		)
-			.catch(err => {
-				console.log(err);
-				res.status(400).json('unable to register');	// (5)
-			});
-	})
+		  	.catch(err => {
+				  console.log(err);
+				  res.status(400).json('Unable to register.');			// (5)
+		   	});
+		})
 }
 
 module.exports = {register};
--- Face Detector (fullstack)/backend/controllers/signIn.js
+++ Face Detector (fullstack)/backend/controllers/signIn.js
@@ -1,25 +1,28 @@
 const signIn = (req, res, db, bcrypt) => {
-	const {email, password} = req.body;
 	const errorMessage = 'Wrong credentials.';
-	db.select('email', 'hash').from('login').where('email', email)
-		.then(data => {
-			bcrypt.compare(password, data[0].hash, (err, isPasswordValid) => {
-				if (isPasswordValid) {
-					db.select().from('users').where('email', email)
-						.then(user => res.json(user[0]))
-						.catch(err => {
-							console.log(err);
-							res.status(400).json('unable to get user');
-						});
-				}
-				else
-					res.status(400).json(errorMessage);
+	const {email, password} = req.body;
+	if (!email || !password)
+		res.status(400).json('Every field must be filled in.');
+	else
+		db.select('email', 'hash').from('login').where('email', email)
+			.then(data => {
+				 bcrypt.compare(password, data[0].hash, (err, isPasswordValid) => {
+					 if (isPasswordValid) {
+						 db.select().from('users').where('email', email)
+							  .then(user => res.json(user[0]))
+							  .catch(err => {
+								  console.log(err);
+								  res.status(400).json('unable to get user');
+							  });
+					 }
+					 else
+						 res.status(400).json(errorMessage);
+				 })
 			})
-		})
-		.catch(err => {
-			console.log(err);
-			res.status(400).json(errorMessage);
-		});
+			.catch(err => {
+				 console.log(err);
+				 res.status(400).json(errorMessage);
+			});
 }
 
 module.exports = {signIn};
